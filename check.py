d55566709c1577851cd1390ec27e596dfe88bc26
skorik-kirill <60388222+skorik-kirill@users.noreply.github.com>
2020-03-16 18:05:37 +0200

    Update check.py

--- check.py
+++ check.py
@@ -1,9 +1,13 @@
-rangeValue = int(input("Enter value for range count"))
-for i in range(rangeValue):
-   if i % 3  == 0 and i % 5 == 0:
+def checkApp():
+ rangeValue = int(input("Enter value for range count"))
+ for i in range(rangeValue):
+    if i % 3  == 0 and i % 5 == 0:
      print(i,' = ab')
-   elif i % 3 == 0:
+    elif i % 3 == 0:
      print(i,' = a')
-   elif i % 5 == 0:
+    elif i % 5 == 0:
      print(i,' = b')
-   else: False
+    else: False
+ return True
+
+checkApp()
