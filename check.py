c15503c5a20f2af731a91d12d8f23eb660355461
skorik-kirill <60388222+skorik-kirill@users.noreply.github.com>
2020-03-16 17:50:25 +0200

    Update check.py

--- check.py
+++ check.py
@@ -1,19 +1,9 @@
-print("range from 0-99")
-for i in range(100):
+rangeValue = int(input("Enter value for range count"))
+for i in range(rangeValue):
    if i % 3  == 0 and i % 5 == 0:
      print(i,' = ab')
    elif i % 3 == 0:
      print(i,' = a')
    elif i % 5 == 0:
      print(i,' = b')
-   else: print(i," = no value")
-print("range from 2-300")
-for i in range(2,300):
-       if i % 3 == 0 and i % 5 == 0:
-           print(i, ' = ab')
-       elif i % 3 == 0:
-           print(i, ' = a')
-       elif i % 5 == 0:
-           print(i, ' = b')
-       else:
-           print(i, " = no value")
+   else: False
